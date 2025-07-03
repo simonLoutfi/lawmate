c2670553e96c4766f3ac9850df0525940522322e
simonLoutfi <simon.loutfi@hotmail.com>
2025-07-03 23:02:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "https://lawmate-chi.vercel.app/",
-  credentials: true // If you're using cookies or auth headers
+  origin: true,  // Reflects the request origin
+  credentials: true
 }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
