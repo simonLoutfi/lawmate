cc2f93fc9ed58c36c95bdb7de1e4f4b1fb72061d
simonLoutfi <simon.loutfi@hotmail.com>
2025-07-04 14:39:36 +0300

    Update api.js

--- lawmate-lebanon-web/src/services/api.js
+++ lawmate-lebanon-web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: 'https://lawmate-fhp7.onrender.com/api',
+  baseURL: 'https://lawmate-hj6p.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
