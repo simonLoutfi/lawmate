21699978823949423818a1fa95378b5fa460098e
simonLoutfi <simon.loutfi@hotmail.com>
2025-07-04 10:09:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -217,5 +217,6 @@ def askai():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
 
